d08bc05abc94c14e16b617ded92f22ca4bdf3be6
Vaishnavi r kannur <kannurvaishnavi@gmail.com>
2025-09-04 18:30:02 +0530

    Add files via upload

---
+++ left half pyr.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,s;
+    printf("Enter n value");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       for(s=1;s<=n-i;s++)
+       {
+       	printf(" ");
+	   }
+	   for(j=1;j<=i;j++)
+	   {
+	   	printf("*");
+	   }
+        printf("\n");
+    }
+}
