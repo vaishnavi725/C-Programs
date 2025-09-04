23ad4396e09913f80b178d9d69f7d7069cc8826d
Vaishnavi r kannur <kannurvaishnavi@gmail.com>
2025-09-04 19:11:14 +0530

    Add files via upload
    This C program takes an integer input n and prints a hollow square pattern of stars (*) with the size n x n. The stars are printed on the borders of the square, and the inside is filled with spaces.

---
+++ Hallow sqaure pattern.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter n value");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        if(j==1||i==1||j==n||i==n)
+        {
+            printf("*");
+        }
+        else
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
